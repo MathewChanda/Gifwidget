a5b1d1a229f5d71aab2c3ef8133ca7d47f961ba9
MathewChanda <ichanda72@gmail.com>
2021-07-22 22:41:26 -0400

    Initial Commit

---
+++ Shared/Gif_widgetApp.swift
@@ -0,0 +1,17 @@
+//
+//  Gif_widgetApp.swift
+//  Shared
+//
+//  Created by Sam Chanda on 7/22/21.
+//
+
+import SwiftUI
+
+@main
+struct Gif_widgetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
