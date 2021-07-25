ec93c15e1b01fbac04269742fb2b24d3ac386bb6
MathewChanda <ichanda72@gmail.com>
2021-07-24 20:54:50 -0400

    forgot what i did lol

---
+++ GifWidget/Views/Home/PreviewItem.swift
@@ -0,0 +1,20 @@
+//
+//  PreviewItem.swift
+//  GifWidget
+//
+//  Created by Sam Chanda on 7/23/21.
+//
+
+import SwiftUI
+
+struct PreviewItem: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct PreviewItem_Previews: PreviewProvider {
+    static var previews: some View {
+        PreviewItem()
+    }
+}
